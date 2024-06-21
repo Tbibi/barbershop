e67d9b95da44e48fa71791fd570a7574464e5a19
LHaitam <lourhmihaitam@outlook.fr>
2024-06-22 00:57:26 +0100

    deploy

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,27 +2,41 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div id="contact" className="max-w-[1240px] mx-auto px-4 py-8 md:py-16">
-      <div className="flex flex-col items-center justify-center mb-8 md:mb-16">
-        <h2 className="text-4xl md:text-5xl mb-2 md:mb-4 text-[#c5ae87] font-sacramento italic">
-          Contactez-Nous
-        </h2>
-        <h3 className="text-3xl md:text-4xl mb-4 md:mb-8 font-semibold text-center">
-          BIENVENUE CHEZ LASER RUSH
-        </h3>
-        <p className="text-lg text-center mb-4 md:mb-8">
-          Pour toute prise de contact ou rendez-vous, veuillez remplir et envoyer ce formulaire ci-dessous ou appeler le{" "}
-          <a
-            style={{ fontFamily: "Raleway" }}
-            className="hover:text-[#C4AF89] transition-transform"
-            href="tel:+33781195599"
+    <div id="contact" className="max-w-[1240px] m-auto p-4 h-screen">
+      <div  className="flex flex-col items-center justify-center">
+        <div>
+          <h2
+            className="text-5xl mb-[-6] text-[#c5ae87] "
+            style={{
+              fontFamily: "Sacramento",
+              fontStyle: "italic",
+            }}
           >
-            0781195599
-          </a>.
-        </p>
+            Contactez-Nous
+          </h2>{" "}
+        </div>
+        <div>
+          <h3 className="text-4xl mt-[-6px] mb-6 font-semibold text-center mx-6">
+            BIENVENUE CHEZ LASER RUSH
+          </h3>
+        </div>
+        <div className="mx-8">
+                <p className="mb-4 text-center text-lg">
+                  Pour toute prise de contact ou rendez-vous, veuillez remplir et envoyer ce formulaire ci-dessus ou appeler le{" "}
+                    <a
+                      style={{
+                        fontFamily: "Raleway",
+                      }}
+                      className="hover:text-[#C4AF89] transition transform"
+                      href="tel:+33781195599"
+                    >
+                      0781195599
+                    </a>.
+                </p>
+                </div>
       </div>
-      <form className="max-w-[600px] mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <form className="max-w-[600px] m-auto mb-[10 rem]">
+        <div className="grid grid-cols-2 gap-2">
           <input
             className="border rounded shadow-lg p-3"
             type="text"
@@ -45,10 +59,10 @@ const Contact = () => {
         <textarea
           className="border rounded shadow-lg p-3 w-full"
           cols="30"
-          rows="5"
+          rows="10"
           placeholder="Message"
         ></textarea>
-        <button className="border border-[#9c8c6d] text-white font-bold bg-[#9c8c6d] rounded shadow-lg p-3 w-full mt-4 transition duration-300 transform hover:scale-105 hover:bg-[#afa38a] hover:text-white">
+        <button className="border border-#9c8c6d text-white font-bold bg-[#9c8c6d] rounded shadow-lg p-3 w-full mt-2 mb-6 transition duration-300 transform hover:scale-105 hover:bg-[#afa38a] hover:text-[white]">
           Envoyer
         </button>
       </form>
